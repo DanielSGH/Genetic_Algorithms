0f46eba76683f3e562942787b741f787429f13eb
DanielSGH <daniel.st2k@gmail.com>
2024-02-08 21:50:27 +0000

    fix partb.py

--- partb.py
+++ partb.py
@@ -5,14 +5,12 @@ import matplotlib.pyplot as plt
 binpackingGApart = binpackingGA()
 ans2 = binpackingGApart.algorithm()
 
-def partb():
-    plt.plot(ans2, 'y')
+plt.plot(ans2, 'y')
 
-    plt.xlabel('Generation')
-    plt.ylabel('Average Fitness')
-    plt.title('Average Fitness of the Population vs Generations')
-    plt.show()
-
-
-if __name__ == "__partb__":
-    partb()
+plt.xlabel('Generation')
+plt.ylabel('Average Fitness')
+plt.title('Average Fitness of the Population vs Generations')
+fig = plt.gcf()
+plt.show()
+plt.draw()
+fig.savefig('fitness.png', dpi=100)
